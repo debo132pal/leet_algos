8313b7ae0ca2f9dcc56ea951cc271b14bca2e95b
debojyoti pal <debo@deboyjoti.pal@gmail.com>
2021-03-07 08:25:11 +0530

    leet 1

--- src/com/algos/MinPathSum.java
+++ src/com/algos/MinPathSum.java
@@ -6,26 +6,26 @@ public class MinPathSum {
         int R = grid.length;
         int C = grid[0].length;
 
-        for( int i = 0 ; i < R ; i++ ){
-            for( int  j = 0 ; j < C ; j++ ) {
+         for( int i = 0 ; i < R ; i++ ){
+             for( int j = 0 ; j < C ; j++ ){
+                 if( i == 0 && j == 0 ){
+                    // do nothing
+                 } else if ( i == 0 ){
+                     grid[i][j] += grid[i][j-1];
+                 } else if ( j == 0 ){
+                     grid[i][j] += grid[i-1][j];
+                 } else
+                    grid[i][j] += Math.min( grid[i-1][j] , grid[i][j-1] );
+             }
+         }
 
-               int top   =  j - 1 >= 0 ? grid[i][j-1] : 0 ;
-               int left  =  i - 1 >= 0 ? grid[i-1][j] : 0;
-               if( top > 0 && left > 0 )
-                    grid[i][j] += Math.min( top, left) ;
-               else if ( top > 0 ) {
-                   grid[i][j] += top;
-               } else if ( left > 0 ){
-                   grid[i][j] += left;
-               }
-            }
-        }
         return grid[R-1][C-1];
     }
 
     public static void main(String[] args) {
         int[][] a =
-                { {1,3,1}, {1,5,1},{4,2,1}};
+                { {1,3,1},
+                  {1,5,1},{4,2,1}};
         MinPathSum p = new MinPathSum();
         System.out.println( p.minPathSum(a));
     }
